84f317e43a2a3b79876174b77e69215868be2f42
TJ Hooker <tyrelhooker@gmail.com>
2021-07-04 15:37:56 -0400

    Begin working on firebase config, seed data, and debugging

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { firebase, FieldValue } from './lib/firebase';
+import FirebaseContext from './context/firebase';
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={{ firebase, FieldValue }}>
     <App />
-  </React.StrictMode>,
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
