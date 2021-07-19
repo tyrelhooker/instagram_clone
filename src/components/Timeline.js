cbb7129714a6fb74054c52d72b1d250bd8a081ab
TJ Hooker <tyrelhooker@gmail.com>
2021-07-19 00:05:56 -0400

    Create logic in firebase services file to add all data to incoming photo object from firestore

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -3,35 +3,30 @@ import Skeleton from 'react-loading-skeleton';
 import useFollowedUsersPhotos from '../hooks/use-followed-users-photos';
 
 export default function Timeline() {
-  const { photos } = useFollowedUsersPhotos();
+  // const { photos } = [];
+  const { photos = {}} = useFollowedUsersPhotos();
   console.log(photos);
-  // const photos = ['1', '2', '3', '4', '5'];
-  // const photos = [];
-  // const photos = null;
+
   return (
     <div className='container col-span-2'>
-      {photos
+      {!photos
         ?
+          <Skeleton count={4} width={320} height={400} className='mb-5' />
+        :
           photos && photos.length > 0 
             ?
-              photos.map((photo, i) => <p key={i}>I will be a photo</p>)
+              photos.map((photo) => (
+                // <>
+                  // <img key={photo.docId} src={photo.imageSrc} alt='Followed User Resource' />
+                // </>
+                <p key={photo.docId}>{photo.username}</p>
+              ))
+
             :
               <p className='text-center text-2xl'>
                 Follow people to see photos!
               </p>
-        :
-          <Skeleton count={1} width={320} height={400} className='mb-5' />
       }
-      {/* <p>This is the timeline.</p>
-      <div className='grid grid-cols-3'>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-      </div> */}
-     
     </div>
 
   )
--- src/hooks/use-followed-users-photos.js
+++ src/hooks/use-followed-users-photos.js
@@ -1,21 +1,31 @@
 import { useState, useEffect, useContext } from "react";
 import UserContext from '../context/user';
+import { getUserByUserId, getUserFollowedPhotos } from "../services/firebase";
 
 export default function useFollowedUsersPhotos() {
   const [photos, setPhotos] = useState(null);
-  const { user: { uid: userId = '' } } = useContext(UserContext);
-  // const userId = uid || '';
+  const { user: { uid: userId = ''} } = useContext(UserContext);
+  
+  
   
   
 
   useEffect(() => {
     async function getTimelinePhotos() {
-    console.log('userId inside getTimelinePhotos', userId);
+      const followingUserIds = await getUserByUserId(userId);
 
+      if (followingUserIds && followingUserIds[0].following.length > 0) {
+        const followedUserPhotos = await getUserFollowedPhotos(userId, followingUserIds[0].following);
+        
+        followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
+        setPhotos(followedUserPhotos);
+        console.log('response in getTimelinePhotos ==>', photos);
+      }
     }
-    if (userId) {
-      getTimelinePhotos();
-    }
+
+   
+    getTimelinePhotos();
+
   }, [userId]);
 
   return { photos };
