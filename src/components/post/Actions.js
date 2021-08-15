1bdec337f59601e4c255a97b074d9bfd9e0b61ba
TJ Hooker <tyrelhooker@gmail.com>
2021-08-15 14:18:53 -0400

    Finish actions component logic to include svg like and comment icons. Clean up code in Header component.

--- src/components/post/Actions.js
+++ src/components/post/Actions.js
@@ -1,35 +1,54 @@
-import React, {useState, useContext } from 'react';
+import {useState, useContext } from 'react';
 import FirebaseContext from '../../context/firebase';
 import UserContext from '../../context/user';
 
 // Need documentId to figure out what document we are working with. It is reference to the document so you can change it. handleFocus - when user clicks on comment icon, this will focus the user to the comment input field.
-export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) {
+export default function Actions({ docId, likes, userLikedPhoto: likedPhoto, handleFocus }) {
+  const {
+    user: { uid: userId ='' }
+  } = useContext(UserContext);
   const [toggleLiked, setToggleLiked] = useState(likedPhoto);
-  const [likes, setLikes] = useState(totalLikes);
+  const [totalLikes, setTotalLikes] = useState(likes.length);
   const { firebase, FieldValue } = useContext(FirebaseContext);
 
-  //TODO: fix this handlToggleLiked logic
+  console.log(likes);
+  console.log(toggleLiked);
+
   const handleToggleLiked = async () => {
-    if (!toggleLiked) {
-      setToggleLiked(true);
-    } else {
-      setToggleLiked(false);
-    }
+    setToggleLiked((toggleLiked) => !toggleLiked);
+    // if (toggleLiked) {
+    //   setToggleLiked(!toggleLiked);
+    //   console.log('insideHandlerToggleLiked', toggleLiked);
+    // } else {
+    //   setToggleLiked(toggleLiked);
+    //   console.log('going from false to true', toggleLiked);
+    // }
+
+    await firebase
+      .firestore()
+      .collection('photos')
+      .doc(docId)
+      .update({
+        likes: toggleLiked ? FieldValue.arrayRemove(userId) : FieldValue.arrayUnion(userId)
+       });
+    
+    setTotalLikes((totalLikes) => (toggleLiked ? totalLikes - 1 : totalLikes + 1));    
   }
 
+  // TODO: fix the liked heart filling black instead of red
   return (
     <>
       <div className='flex justify-between p-4'>
         <div className='flex'>
           <svg
-            onClick={() => handleToggleLiked((toggleLiked) => !toggleLiked)}
+            onClick={() => handleToggleLiked((toggleLiked) => !toggleLiked )}
             onKeyDown={(e) => {
               if (e.key === 'Enter') {
                   handleToggleLiked((toggleLiked) => !toggleLiked);
               }
             }}
             className={`w-8 mr-4 select-none cursor-pointer ${
-                toggleLiked ? 'fill-red text-red' : 'text-black'
+                toggleLiked ? 'fill-current text-red' : 'text-red'
             }`}
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -44,7 +63,31 @@ export default function Actions({ docId, totalLikes, likedPhoto, handleFocus }) 
               d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
             />
           </svg>
+          <svg
+            onClick={handleFocus}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleFocus();
+              }
+            }}
+            className='w-8 text-black-light select-none cursor-pointer'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            tabIndex={0}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d='M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z'
+            />
+          </svg>
         </div>
+      </div>
+      <div className='p-4 py-0'>
+        <p className='font-bold'>{totalLikes === 1 ? `${totalLikes} like` : `${totalLikes} likes`}</p>
       </div>
     </>
   )
--- src/components/post/Header.js
+++ src/components/post/Header.js
@@ -1,17 +1,17 @@
-import React from 'react';
+// import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Header({ content: {username} }) {
   return (
-    <div className='flex text-center items-center my-2'>
-      <Link to={`/p/${username}`} className='pr-2'>
+    <div className='flex border-b items-center h-4 p-4 py-9'>
+      <Link to={`/p/${username}`} className='flex items-center'>
         <img
-          className='flex rounded-full h-8 w-8'
+          className='flex rounded-full h-8 w-8 mr-3'
           src={`/images/avatars/${username}.jpg`}
           alt={`${username} profile avatar`}
         />
       </Link>
-      {username}
+      <p className='font-bold'>{username}</p>
     </div>
   )
 }
